0ed5775d8a0bf4970fa9ccc2694197149e313ab0
Taivanjargal <uulaa2@gmail.com>
2020-09-05 19:39:09 +0800

    Toglogchiin bairiig solih

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -21,11 +21,32 @@ document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+//shoog shideh  event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 sanamsargui toog avna
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //shoonii zurgiig delgets deer gargana
   diceDom.style.display = "block";
+  //buusan sanamsargui  toond hargalzah zurgiig gargah
   diceDom.src = "dice-" + diceNumber + ".png";
+  // buusan too 1-s yalgaatai bol toglogchhin eeljiin onoog uurchilnu (nemegduulne)
+  if (diceNumber != 1) {
+    //1-s yalgaatai too buulaa. Buusan toog toglogchid nemne
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //1 buusan bol toglogchiin eeliig solino
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // toglogchii eeljiig nuguu toglogch ruu shiljuulne
+    //herev idevhitei toglogch ni 0 baival  idevhitei toglogschiig 1 bolgo ugui bol 0 bolgo
+    activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //idevhitei toglogchiig  shiljuuleh (ulaan tseg)
 
-  //   alert("shoo buulaa:" + diceNumber);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    
+    //shoog tur alga bolgono
+    diceDom.style.display="none";
+  }
 });
